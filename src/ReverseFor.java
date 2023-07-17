bb0853c9859b105e3249d6b536c8bbab6ce4fddb
AdityaS98 <adityasharmalko1998@gmail.com>
2023-07-17 12:01:49 +0530

    Adding a program for calculating reverse number with for loop

---
+++ src/ReverseFor.java
@@ -0,0 +1,21 @@
+package com.bascicprograms;
+
+import java.util.Scanner;
+
+public class ReverseFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+
+
+        int sum = 0;
+
+        for (int num = sc.nextInt(); num > 0; num = num / 10) {
+            int r = num % 10;
+            sum = sum * 10 + r;
+
+        }
+        System.out.println(sum);
+    }
+}
